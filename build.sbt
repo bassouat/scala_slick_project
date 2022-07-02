ea203107c92723618e68b030f30c79ee24a3cc12
Basseydou Ouattara <bassouat8@gmail.com>
2022-07-02 08:02:40 +0200

    creating and initializing scala project

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Scala_Slick_CRUD_Project"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
---
+++ src/main/scala/com/slick_crud/Main.scala
@@ -0,0 +1,9 @@
+package com.slick_crud
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
